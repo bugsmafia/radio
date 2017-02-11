b1566db5f4e2a7c90db3855ade5e3999bf510171
Сергей <bugsmafia@gmail.com>
2017-02-11 11:23:10 +0500

    1

---
+++ js/StatusBar.js
@@ -0,0 +1,45 @@
+MusicControls.destroy(onSuccess, onError);
+function StatusBarEvents(action) {
+    switch(action) {
+        case 'music-controls-pause':
+            // Нажата пауза
+			console.log('Нажата пауза');
+            break;
+        case 'music-controls-play':
+            // Нажат Плей
+			console.log('Нажат Плей');
+            break;
+        case 'music-controls-destroy':
+            // Закрыли
+			console.log('Закрыли');
+            break;
+
+        // Headset events (Android only)
+        // All media button events are listed below
+        case 'music-controls-media-button' :
+            // Do something
+			console.log('music-controls-media-button');
+            break;
+        case 'music-controls-headset-unplugged':
+            // Do something
+			console.log('music-controls-headset-unplugged');
+            break;
+        case 'music-controls-headset-plugged':
+            // Do something
+			console.log('music-controls-headset-plugged');
+            break;
+        default:
+            break;
+    }
+}
+
+// Register callback
+MusicControls.subscribe(StatusBarEvents);
+MusicControls.listen();
+
+MusicControls.create({
+	track       : 'Time is Running Out',
+	artist      : 'Muse',
+    cover       : 'icon.png',
+	isPlaying   : true
+}, onSuccess, onError);
